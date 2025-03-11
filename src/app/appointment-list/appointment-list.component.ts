3c3fded58c9405bc026b3ae578b66e42a1d4c0e1
Geoffrey Brophy <geoffrey.brophy@proton.me>
2025-03-10 20:56:16 -0400

    Initial Commit

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-appointment-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+
+}
