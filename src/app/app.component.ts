c6d70a78e39f1bd57c45b7b08f94b1f0e67c8f05
Geoffrey Brophy <geoffrey.brophy@proton.me>
2025-03-10 20:57:36 -0400

    Intial Project Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {AppointmentListComponent} from "./appointment-list/appointment-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppointmentListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
