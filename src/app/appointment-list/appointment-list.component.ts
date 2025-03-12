9f1039f7e9de4cdea625bbda82d82cefa038d2e9
Geoffrey Brophy <geoffrey.brophy@proton.me>
2025-03-12 10:50:42 -0400

    Added one-way data binding

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
@@ -9,5 +10,9 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
-
+  appointment: Appointment = {
+    id: 1,
+    title: "Take dog for a walk",
+    date: new Date(),
+  }
 }
